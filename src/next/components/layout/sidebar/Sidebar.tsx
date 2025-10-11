6a34215de46deb2d53bc2791cc4fd9a325589c88
Armando Vasquez <mando@mcnuggies.dev>
2025-10-11 16:24:01 -0500

    use bootstrap-list icon instead of inline SVGs

--- src/next/components/layout/sidebar/Sidebar.tsx
+++ src/next/components/layout/sidebar/Sidebar.tsx
@@ -4,6 +4,7 @@ import kebabCase from '../../../util/kebabCase'
 import { toggleShowSidebar } from "../../../redux/sidebarSlice"
 import styles from './Sidebar.module.scss'
 import SidebarOption, { SidebarOptionConfig } from './SidebarOption'
+import CustomIcon from '../../shared/CustomIcon'
 
 interface SidebarProps {
     options: SidebarOptionConfig[]
@@ -106,21 +107,11 @@ const Sidebar: FunctionComponent<SidebarProps> = () => {
           onClick={() => dispatch(toggleShowSidebar())}
           aria-label={isSidebarOpen ? `Close sidebar` : `Open sidebar`}
         >
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M4 6h16M4 12h16M4 18h16"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
+          <CustomIcon
+              fileName={`bootstrap-list`}
+              height={20}
+              width={20}
             />
-          </svg>
         </button>
       </div>
     )
